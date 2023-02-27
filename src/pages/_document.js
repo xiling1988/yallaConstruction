abfdff59580249d8f53eff1d35231bfcceb69f60
hans schilling <hasp1988@gmail.com>
2023-02-27 12:51:58 +0400

    ready to deploy

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className='h-full' lang='en'>
       <Head />
-      <body>
+      <body className='h-full'>
         <Main />
         <NextScript />
       </body>
